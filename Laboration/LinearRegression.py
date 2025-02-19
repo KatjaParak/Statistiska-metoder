bbb30b035d3f64951ed74a0b36ee95d16221419d
KatjaParak <katsiaryna.parakhnevich@iths.se>
2025-02-19 22:10:22 +0100

    update:added functions

--- Laboration/LinearRegression.py
+++ Laboration/LinearRegression.py
@@ -3,13 +3,13 @@ import scipy.stats as stats
 
 
 class LinearRegression:
-    def __init__(self, Y, X):
-        self.Y = Y
-        self.X = np.column_stack([np.ones(Y.shape[0]), X])
+    def __init__(self, y, X):
+        self.y = y
+        self.X = np.column_stack([np.ones(y.shape[0]), X])
 
     @property
     def fit(self):  # estimates coefficients
-        return np.linalg.pinv(self.X.T @ self.X) @ self.X.T @ self.Y
+        return np.linalg.pinv(self.X.T @ self.X) @ self.X.T @ self.y
 
     @property
     def d(self):  # contains the number of features
@@ -17,11 +17,11 @@ class LinearRegression:
 
     @property
     def n(self):  # contains the size of the sample
-        return self.Y.shape[0]
+        return self.y.shape[0]
 
     @property
     def SSE(self):  # estimates the amount of variability that can not be explained by the linear model
-        return np.sum(np.square(self.Y - (self.X @ self.fit)))
+        return np.sum(np.square(self.y - (self.X @ self.fit)))
 
     @property
     def variance(self):  # calculates the variance
@@ -33,7 +33,7 @@ class LinearRegression:
 
     @property
     def Syy(self):  # calculates the totat variability
-        return (self.n*np.sum(np.square(self.Y)) - np.square(np.sum(self.Y)))/self.n
+        return (self.n*np.sum(np.square(self.y)) - np.square(np.sum(self.y)))/self.n
 
     @property
     def SSR(self):  # calculates the sum of squares due to regression
@@ -48,9 +48,31 @@ class LinearRegression:
         return stats.f.sf(self.significance_stat, self.d, self.n-self.d-1)
 
     @property
-    def significance_param(self):
+    def c(self):
         return np.linalg.pinv(self.X.T @ self.X)*self.variance
 
     @property
     def Rsq(self):  # estimates the extent to which the model fits the data
         return self.SSR/self.Syy
+
+    @property
+    def confidence_level(self):
+        return round(self.Rsq, 3)
+
+    @property  # calculates the Pearson correlation between all pairs of parameters
+    def individuall_corr(self):
+        return [stats.pearsonr(self.X[:, i], self.X[:, j])[:][0]
+                for i in range(1, self.d+1) for j in range(i+1, self.d+1) if i != j]
+
+    @property
+    def individuall_signif(self):
+        return [2*min(stats.t.cdf(self.fit[i]/(self.std_deviation*np.sqrt(self.c[i, i])), self.n-self.d-1),
+                      stats.t.sf(self.fit[i]/(self.std_deviation*np.sqrt(self.c[i, i])), self.n-self.d-1))
+                for i in range(1, self.d+1)]
+
+    @property
+    def print_p_value(self):
+        features = ['Kinematic', 'Geometric', 'Inertial', 'Observer']
+
+        for f, val in zip(features, self.individuall_signif):
+            print(f"P-value for {f}: {val}")
